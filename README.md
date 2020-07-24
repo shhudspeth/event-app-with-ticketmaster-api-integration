208f7655eadc131eddc43621b6c082349c176da7
Sarah Hudspeth <shhudspeth@gmail.com>
2020-07-24 23:52:15 +0000

    udpated json

---
+++ README.md
@@ -0,0 +1,1 @@
+a hackbright flask app that integrates ticketmaster's api for event planning purposes
--- server.py
+++ server.py
@@ -41,7 +41,8 @@ def find_afterparties():
     sort = request.args.get('sort', '')
 
     url = 'https://app.ticketmaster.com/discovery/v2/events'
-    payload = {'apikey': API_KEY}
+    payload = {'apikey': API_KEY, 'keyword':keyword, 'postalCode':postalcode, 
+                'radius': radius, 'unit':unit, 'sort':sort}
 
     # TODO: Make a request to the Event Search endpoint to search for events
     #
@@ -54,9 +55,14 @@ def find_afterparties():
     # - Replace the empty list in `events` with the list of events from your
     #   search results
 
-    data = {'Test': ['This is just some test data'],
-            'page': {'totalElements': 1}}
-    events = []
+    
+
+    data = requests.get(url, params=payload).json()
+    print(data.keys())
+    if '_embedded' in data:
+        events = data['_embedded']['events']
+    else:
+        events=[]
 
     return render_template('search-results.html',
                            pformat=pformat,
